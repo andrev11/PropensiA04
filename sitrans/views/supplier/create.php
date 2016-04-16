7b80b4b3c07bde0ff2adc3e8c434c12817832739
akramam <akram-amr@hotmail.com>
2016-04-16 16:10:09 +0700

    fix s supplier
    yes

--- sitrans/views/supplier/create.php
+++ sitrans/views/supplier/create.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 /* @var $model app\models\Supplier */
 
 $this->title = Yii::t('app', 'Create Supplier');
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Suppliers'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Supplier'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="supplier-create">
