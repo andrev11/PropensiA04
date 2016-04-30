1f180a21fa507bd94b4416926bc81daeeb5a0be2
bintinrc <bintinurcahya@gmail.com>
2016-05-01 00:07:25 +0700

    Daftar Re-order di home purchasing

--- sitrans/views/site/index.php
+++ sitrans/views/site/index.php
@@ -2,6 +2,8 @@
 
 /* @var $this yii\web\View */
 
+use yii\grid\GridView;
+use app\controllers\SiteController;
 $this->title = 'Dashboard';
 ?>
 <div class="site-index">
@@ -15,7 +17,37 @@ $this->title = 'Dashboard';
                               $info[] = (\Yii::$app->user->identity->nama);
 
                           echo implode($info);
+
         ?>!</h1>
         
-    </div>
+        <?php 
+        	if(Yii::$app->user->identity->role=='purchasing'){
+        		echo "<br>"; 
+        		echo "<h3>Daftar Re-Order</h3> <br>"; 
+        		echo "<table class='table table-striped table-bordered'>"; 
+		        echo "<thead>";
+		         echo "<th>#</th>";
+		        echo "<th>Nama Jenis</th>";
+		        echo "<th>Stok Kilo</th>";
+		        echo "<th>Stok Karton</th>";
+		        echo "<th>ROP</th>";
+		        echo "</thead>";
+				echo "<tbody>"; 
+					echo SiteController::connect();
+	 				$query=pg_query("Select * from jenis where stok_kilo < rop;");
+	 				$count=0;
+		        	while($value = pg_fetch_array($query)){
+		        		$count++;
+		        		echo "<td align='left'>".$count."</td>";
+						echo "<td align='left'>".$value['namajenis']."</td>";
+						echo "<td align='left'>".$value['stok_kilo']."</td>";
+						echo "<td align='left'>".$value['stok_karton']."</td>";
+						echo "<td align='left'>".$value['rop']."</td>";
+						echo "</tr>";
+					}
+				echo "</tbody>";  
+		    	echo "</table>";
+			} 
+		?>        
+ 	</div>
 </div>
