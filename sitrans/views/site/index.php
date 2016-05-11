7eea5e432442b26a1341727c8a06c9993a2301a9
andrev11 <andrevalerian@yahoo.com>
2016-05-11 11:38:46 +0700

    hem fix

--- sitrans/views/site/index.php
+++ sitrans/views/site/index.php
@@ -21,37 +21,39 @@ $this->title = 'Dashboard';
                           echo implode($info);
 
         ?>!</h1>
-         <?php
-         if(Yii::$app->user->identity->role=='bod'){
-         
-         echo SiteController::connect();
+		
+        <?php
+			if(Yii::$app->user->identity->role=='bod'){
+			 
+				echo SiteController::connect();
 
-         $result = pg_query("select date_trunc('month', pembayaran_in.tgl_bayar), sum(pembayaran_in.jumlahbayar) as pembayaran_in, sum(pembayaran_out.jumlahbayar) as pembayaran_out from pembayaran_in FULL OUTER JOIN pembayaran_out ON date_trunc('month', pembayaran_in.tgl_bayar) = date_trunc('month', pembayaran_out.tgl_bayar) WHERE date_trunc('month', pembayaran_in.tgl_bayar) BETWEEN date_trunc('month', current_timestamp - interval '1 year') AND date_trunc('month', current_timestamp) GROUP BY 1 ORDER BY 1;");
+				$result = pg_query("select date_trunc('month', pembayaran_in.tgl_bayar), sum(pembayaran_in.jumlahbayar) as pembayaran_in, sum(pembayaran_out.jumlahbayar) as pembayaran_out from pembayaran_in FULL OUTER JOIN pembayaran_out ON date_trunc('month', pembayaran_in.tgl_bayar) = date_trunc('month', pembayaran_out.tgl_bayar) WHERE date_trunc('month', pembayaran_in.tgl_bayar) BETWEEN date_trunc('month', current_timestamp - interval '1 year') AND date_trunc('month', current_timestamp) GROUP BY 1 ORDER BY 1;");
 
-         $kategori = array();
-         $pembayaran_in = array();
-         $pembayaran_out = array();
-         while($row = pg_fetch_assoc($result)) {
-         	array_push($kategori, date("F Y", strtotime($row['date_trunc'])));
-         	array_push($pembayaran_in, (int)$row['pembayaran_in']);
-         	array_push($pembayaran_out, (int)$row['pembayaran_out']);
-         }
-         
+				$kategori = array();
+				$pembayaran_in = array();
+				$pembayaran_out = array();
+				while($row = pg_fetch_assoc($result)) {
+					array_push($kategori, date("F Y", strtotime($row['date_trunc'])));
+					array_push($pembayaran_in, (int)$row['pembayaran_in']);
+					array_push($pembayaran_out, (int)$row['pembayaran_out']);
+				}
+				 
 
-        echo Highcharts::widget([
-			'options'=>[
-				'title' => ['text' => 'Data Pendapatan dan Pengeluaran PT. HIJRAH GIZI HEWANI'],
-				'xAxis' => ['categories' => $kategori],
-				'yAxis' => ['title' => ['text' => 'Dalam Juta Rupiah']],
-				'series' => [
-								['name' => 'Pendapatan', 'data' => $pembayaran_in, 'color' => '#0000FF'],
-								['name' => 'Pengeluaran', 'data' => $pembayaran_out, 'color' => '#000000']
-							]
-			]
-	]);
+				echo Highcharts::widget([
+					'options'=>[
+						'title' => ['text' => 'Data Pendapatan dan Pengeluaran PT. HIJRAH GIZI HEWANI'],
+						'xAxis' => ['categories' => $kategori],
+						'yAxis' => ['title' => ['text' => 'Dalam Juta Rupiah']],
+						'series' => [
+										['name' => 'Pendapatan', 'data' => $pembayaran_in, 'color' => '#0000FF'],
+										['name' => 'Pengeluaran', 'data' => $pembayaran_out, 'color' => '#000000']
+									]
+					]
+				]);
 			}
-			?>
-		        <?php 
+		?>
+		
+		<?php 
         	if(Yii::$app->user->identity->role=='purchasing'){
         		echo "<br>"; 
         		echo "<h3>Daftar Re-Order</h3> <br>"; 
@@ -80,7 +82,6 @@ $this->title = 'Dashboard';
 		    	echo "</table>";
 			} 
 		?> 
-    </div>        
-       
+              
  	</div>
 </div>
